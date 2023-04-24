55d6aba96b97d1b7c338a8d05389ef4385697db5
Marcelo Rubini <rubini.marcelo@gmail.com>
2023-04-24 04:32:12 -0300

    fixes Validators view Info: transactions signed by period of time chart and each validator box info

--- app/src/components/validators/index.tsx
+++ app/src/components/validators/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import { BridgeValidator } from '@/src/components/validators/BridgeValidator'
-import { TransactionsSigned } from '@/src/components/validators/TransactionsSigned'
+import {
+  TransactionsSigned,
+  weekAgoTimestamp,
+} from '@/src/components/validators/TransactionsSigned'
 import { Bridges } from '@/src/constants/config/bridges'
-import { useFetchValidators } from '@/src/hooks/subgraph/useValidators'
+import {
+  useFetchValidators,
+  useFetchValidatorsExecutions,
+  useFetchValidatorsSignatures,
+} from '@/src/hooks/subgraph/useValidators'
 
 const Columns = styled.div`
   display: grid;
@@ -43,116 +51,37 @@ const Chart = styled(TransactionsSigned)`
   display: none;
 `
 
+const dayAgoTimestamp = () => {
+  const now = new Date()
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1).getTime() / 1000
+}
+
+type SigsCount = {
+  name: string
+  value: number
+}
+type ExecsCount = {
+  name: string
+  value: number
+}
+
 export const BridgeValidators: React.FC = () => {
   const { validators: xdaiValidators } = useFetchValidators(Bridges.xdai)
   const { validators: ambValidators } = useFetchValidators(Bridges.amb)
-
-  const data = [
-    [
-      {
-        name: 'Gnosis Safe',
-        value: 3000,
-      },
-      {
-        name: 'Protofire',
-        value: 3000,
-      },
-      {
-        name: 'Cow Protocol',
-        value: 1500,
-      },
-      {
-        name: 'Gnosis DAO',
-        value: 2500,
-      },
-      {
-        name: 'Giveth',
-        value: 800,
-      },
-      {
-        name: 'Kartpatkey',
-        value: 1209,
-      },
-    ],
-    [
-      {
-        name: 'Gnosis Safe',
-        value: 10000,
-      },
-      {
-        name: 'Protofire',
-        value: 12000,
-      },
-      {
-        name: 'Cow Protocol',
-        value: 5000,
-      },
-      {
-        name: 'Gnosis DAO',
-        value: 1000,
-      },
-      {
-        name: 'Giveth',
-        value: 500,
-      },
-      {
-        name: 'Kartpatkey',
-        value: 7500,
-      },
-    ],
-    [
-      {
-        name: 'Gnosis Safe',
-        value: 25839,
-      },
-      {
-        name: 'Protofire',
-        value: 50000,
-      },
-      {
-        name: 'Cow Protocol',
-        value: 90203,
-      },
-      {
-        name: 'Gnosis DAO',
-        value: 30929,
-      },
-      {
-        name: 'Giveth',
-        value: 19029,
-      },
-      {
-        name: 'Kartpatkey',
-        value: 86000,
-      },
-    ],
-    [
-      {
-        name: 'Gnosis Safe',
-        value: 10000000,
-      },
-      {
-        name: 'Protofire',
-        value: 7040923,
-      },
-      {
-        name: 'Cow Protocol',
-        value: 8829000,
-      },
-      {
-        name: 'Gnosis DAO',
-        value: 8321000,
-      },
-      {
-        name: 'Giveth',
-        value: 5902001,
-      },
-      {
-        name: 'Kartpatkey',
-        value: 7500000,
-      },
-    ],
-  ]
+  const [xdaiTimePeriod, setXDAITimePeriod] = useState(weekAgoTimestamp())
+  const [ambTimePeriod, setAMBTimePeriod] = useState(weekAgoTimestamp())
+  const xdaiSignedTXs = useFetchValidatorsSignatures('XDAI', xdaiTimePeriod)
+  const ambSignedTXs = useFetchValidatorsSignatures('AMB', ambTimePeriod)
+  const xdaiTodaysSignedTXs = useFetchValidatorsSignatures('XDAI', dayAgoTimestamp())
+  const ambTodaysSignedTXs = useFetchValidatorsSignatures('AMB', dayAgoTimestamp())
+  const xdaiTodaysExecutedTXs = useFetchValidatorsExecutions('XDAI', dayAgoTimestamp())
+  const ambTodaysExecutedTXs = useFetchValidatorsExecutions('AMB', dayAgoTimestamp())
+  if (!xdaiSignedTXs.data && xdaiSignedTXs.error) {
+    throw new Error('No data for XDAI Signed Transactions')
+  }
+  if (!ambSignedTXs.data && ambSignedTXs.error) {
+    throw new Error('No data for XDAI Signed Transactions')
+  }
 
   return (
     <>
@@ -160,19 +89,47 @@ export const BridgeValidators: React.FC = () => {
         xDai Bridge Validators <TitleNote>(Ethereum-Gnosis Chain)</TitleNote>
       </Title>
       <Columns>
-        <Chart data={data} />
-        {xdaiValidators.map((validator, index) => (
-          <BridgeValidator bridgeValidator={validator} key={`validator_${index}`} />
-        ))}
+        <Chart data={xdaiSignedTXs.data ?? []} onTimePeriodChange={setXDAITimePeriod} />
+        {xdaiValidators.map((validator, index) => {
+          const todaysSignatures = xdaiTodaysSignedTXs.data?.find((signaturesCount: SigsCount) => {
+            return signaturesCount.name === validator.name
+          })
+          const todaysExecutions = xdaiTodaysExecutedTXs.data?.find(
+            (executionsCount: ExecsCount) => {
+              return executionsCount.name === validator.name
+            },
+          )
+          validator.signed = todaysSignatures?.value ?? 0
+          validator.executed = todaysExecutions?.value ?? 0
+          return (
+            <>
+              <BridgeValidator bridgeValidator={validator} key={`validator_${index}`} />
+            </>
+          )
+        })}
       </Columns>
       <Title style={{ paddingTop: '24px' }}>
         AMB Bridge Validators <TitleNote>(Ethereum-Gnosis Chain)</TitleNote>
       </Title>
       <Columns>
-        <Chart data={data} />
-        {ambValidators.map((validator, index) => (
-          <BridgeValidator bridgeValidator={validator} key={`validator_${index}`} />
-        ))}
+        <Chart data={ambSignedTXs.data ?? []} onTimePeriodChange={setAMBTimePeriod} />
+        {ambValidators.map((validator, index) => {
+          const todaysSignatures = ambTodaysSignedTXs.data?.find((signaturesCount: SigsCount) => {
+            return signaturesCount.name === validator.name
+          })
+          const todaysExecutions = ambTodaysExecutedTXs.data?.find(
+            (executionsCount: ExecsCount) => {
+              return executionsCount.name === validator.name
+            },
+          )
+          validator.signed = todaysSignatures?.value ?? 0
+          validator.executed = todaysExecutions?.value ?? 0
+          return (
+            <>
+              <BridgeValidator bridgeValidator={validator} key={`validator_${index}`} />
+            </>
+          )
+        })}
       </Columns>
     </>
   )
