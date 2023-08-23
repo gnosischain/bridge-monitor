4a07410482a891f54c1c36d1d750c347e0a5d804
Gabito Esmiapodo <gabriel@bootnode.dev>
2023-08-23 13:15:33 -0300

    change: basePath

--- app/next.config.js
+++ app/next.config.js
@@ -1,32 +1,17 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import("next").NextConfig} */
 module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
+  reactStrictMode: false,
+  basePath: '/app',
   i18n: {
-    // ie
-    // locales: ['en', 'es'],
     locales: ['en'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
   },
   images: {
     unoptimized: true,
-    domains: [
-      "tokens.1inch.io",
-      "assets.coingecko.com",
-      "ethereum-optimism.github.io"
-    ]
+    domains: ['tokens.1inch.io', 'assets.coingecko.com', 'ethereum-optimism.github.io'],
   },
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     '/': { page: '/' },
-  //     // '/bridges': { page: '/bridges', query: { title: 'bridges' } },
-  //     // '/validators': { page: '/validators', query: { title: 'lvalidators' } },
-  //   }
-  // },
-});
+})
