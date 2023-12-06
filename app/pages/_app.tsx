be015497abcaf4d36ee2e18770e29dc97971b073
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2023-12-06 16:43:41 -0300

    fix: scroll issue (#570)

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next'
 import type { AppContext, AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import NextApp from 'next/app'
 import dynamic from 'next/dynamic'
-import { ReactElement, ReactNode } from 'react'
-
+import { ReactElement, ReactNode, useEffect } from 'react'
 import { AbstractIntlMessages, NextIntlProvider } from 'next-intl'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 import { SWRConfig } from 'swr'
-
 import SafeSuspense from '@/src/components/helpers/SafeSuspense'
 import { Layout } from '@/src/components/layout'
 import { Footer } from '@/src/components/layout/Footer'
@@ -19,6 +18,7 @@ import ThemeProvider from '@/src/providers/themeProvider'
 import { intlErrorHandler } from '@/src/utils/intlErrorHandler'
 import TooltipConfig from '@/src/components/tooltip/TooltipConfig'
 import { ValidatorsProvider } from '@/src/providers/validatorsProvider'
+import { useRef } from 'react'
 
 import 'sanitize.css'
 import 'react-tooltip/dist/react-tooltip.css'
@@ -44,6 +44,57 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, messages, pageProps }: AppPropsWithLayout) {
   // Black magic explained here https://nextjs.org/docs/basic-features/layouts
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
+  const router = useRouter()
+
+  const scrollCache = useRef<Record<string, [number, number]>>({})
+  const activeRestorePath = useRef<string>()
+
+  useEffect(() => {
+    if (history.scrollRestoration !== 'manual') {
+      history.scrollRestoration = 'manual'
+    }
+    const getCurrentPath = () => location.pathname + location.search
+    router.beforePopState(() => {
+      activeRestorePath.current = getCurrentPath()
+      return true
+    })
+    const onComplete = () => {
+      const scrollPath = activeRestorePath.current
+      if (!scrollPath || !(scrollPath in scrollCache.current)) {
+        window.scrollTo(0, 0)
+        return
+      }
+
+      activeRestorePath.current = undefined
+      const [scrollX, scrollY] = scrollCache.current[scrollPath]
+      window.scrollTo(scrollX, scrollY)
+      // sometimes rendering the page can take a bit longer
+      const delays = [30, 60, 120, 240, 480]
+      const checkAndScroll = () => {
+        if (
+          (window.scrollX === scrollX && window.scrollY === scrollY) ||
+          scrollPath !== getCurrentPath()
+        ) {
+          return
+        }
+        window.scrollTo(scrollX, scrollY)
+        const delay = delays.shift()
+        if (delay) {
+          setTimeout(checkAndScroll, delay)
+        }
+      }
+      setTimeout(checkAndScroll, delays.shift())
+    }
+    const onScroll = () => {
+      scrollCache.current[getCurrentPath()] = [window.scrollX, window.scrollY]
+    }
+    router.events.on('routeChangeComplete', onComplete)
+    window.addEventListener('scroll', onScroll)
+    return () => {
+      router.events.off('routeChangeComplete', onComplete)
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [router])
 
   return (
     <>
