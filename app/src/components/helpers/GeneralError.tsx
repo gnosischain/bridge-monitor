0032d55d0042925adaa036e0467990b45afaecd8
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2023-12-15 14:31:22 -0300

    add: better general error (#584)

--- app/src/components/helpers/GeneralError.tsx
+++ app/src/components/helpers/GeneralError.tsx
@@ -1,11 +1,46 @@
+import styled from 'styled-components'
+
 import { FallbackProps } from 'react-error-boundary'
+
+import { Alert } from '@/src/components/assets/Alert'
+import { ButtonPrimary } from '@/src/components/buttons/Button'
+import { GenericError } from '@/src/components/common/GenericError'
+
+const Button = styled(ButtonPrimary)`
+  margin: 0 auto;
+  font-size: 1.5rem;
+  padding: 0 16px;
+  height: 50px;
+  width: 100%;
+`
+
+const Code = styled.blockquote`
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  font-size: 1.6rem;
+  font-style: italic;
+  line-height: 1.5;
+  margin: 0 0 16px;
+  padding: 12px 10px;
+`
 
 export const GeneralError = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <div role="alert">
-      <div>Oh no</div>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+    <GenericError
+      icon={<Alert />}
+      text={
+        <>
+          <Code>
+            <b>Error:</b> {error.message}
+          </Code>
+          <Button onClick={resetErrorBoundary}>Try again</Button>
+        </>
+      }
+      title="Something went wrong"
+    />
   )
 }
