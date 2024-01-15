54954813114430cb32545eb7da7b09cbe937667e
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2024-01-15 16:45:51 -0300

    feat: datepicker styles (#631)
    * update: datepicker styles
    
    * fix: border and border radius

--- app/src/theme/datePicker.tsx
+++ app/src/theme/datePicker.tsx
@@ -3,26 +3,35 @@ import { css } from 'styled-components'
 export const datePickerCSS = css`
   :root {
     .react-datepicker {
-      font-size: 1.1rem;
+      --datepicker-border-radius: 8px;
+
+      font-size: 1.2rem;
+      background-color: ${({ theme }) => theme.colors.cream};
       border: none;
-      box-shadow: none;
+      border-radius: var(--datepicker-border-radius);
+      box-shadow: 0 51px 80px rgba(0, 0, 0, 0.17), 0 19.6444px 25.4815px rgba(0, 0, 0, 0.103259),
+        0 4.15556px 6.51852px rgba(0, 0, 0, 0.0667407);
     }
 
     .react-datepicker__month-container {
-      background-color: ${({ theme }) => theme.colors.darkerGrey};
+      background-color: ${({ theme }) => theme.colors.cream};
+      border-radius: var(--datepicker-border-radius);
+      border-radius: var(--datepicker-border-radius);
       padding-bottom: var(--theme-common-space);
     }
 
     .react-datepicker__header {
-      background-color: ${({ theme }) => theme.colors.primary};
-      color: ${({ theme }) => theme.colors.cream};
+      background-color: ${({ theme }) => theme.colors.creamDark};
+      border-top-left-radius: var(--datepicker-border-radius);
+      border-top-right-radius: var(--datepicker-border-radius);
+      color: ${({ theme }) => theme.colors.primary};
       border: none;
       padding: calc(var(--theme-common-space) * 2) 0 var(--theme-common-space);
     }
 
     .react-datepicker-popper[data-placement^='bottom'] .react-datepicker__triangle::before,
     .react-datepicker-popper[data-placement^='bottom'] .react-datepicker__triangle::after {
-      border-bottom-color: ${({ theme }) => theme.colors.primary};
+      border-bottom-color: ${({ theme }) => theme.colors.creamDark};
     }
 
     .react-datepicker__day-name,
@@ -40,12 +49,31 @@ export const datePickerCSS = css`
     .react-datepicker-year-header,
     .react-datepicker__day-name,
     .react-datepicker__time-name {
-      color: ${({ theme }) => theme.colors.cream};
+      color: ${({ theme }) => theme.colors.primary};
       font-size: 1.2rem;
     }
 
+    .react-datepicker__navigation-icon::before {
+      border-color: ${({ theme }) => theme.colors.primary};
+    }
+
     .react-datepicker__day {
-      color: ${({ theme }) => theme.colors.creamDark};
+      color: ${({ theme }) => theme.colors.primary};
+
+      &.react-datepicker__day--today:not(.react-datepicker__day--selected) {
+        background-color: ${({ theme }) => theme.colors.white};
+        color: ${({ theme }) => theme.colors.primary};
+        font-weight: 700;
+      }
+
+      &.react-datepicker__day--disabled {
+        &,
+        &:hover {
+          background-color: transparent;
+          color: ${({ theme }) => theme.colors.primary};
+          opacity: 0.5;
+        }
+      }
     }
 
     .react-datepicker__day--selected,
@@ -64,8 +92,8 @@ export const datePickerCSS = css`
     .react-datepicker__month-text--keyboard-selected,
     .react-datepicker__quarter-text--keyboard-selected,
     .react-datepicker__year-text--keyboard-selected {
-      background-color: ${({ theme }) => theme.colors.secondary};
-      color: ${({ theme }) => theme.colors.darkerGrey};
+      background-color: ${({ theme }) => theme.colors.primary};
+      color: ${({ theme }) => theme.colors.cream};
       font-weight: 700;
     }
 
@@ -73,7 +101,8 @@ export const datePickerCSS = css`
     .react-datepicker__month-text:hover,
     .react-datepicker__quarter-text:hover,
     .react-datepicker__year-text:hover {
-      background-color: ${({ theme }) => theme.colors.primary};
+      background-color: ${({ theme }) => theme.colors.primaryDark};
+      color: ${({ theme }) => theme.colors.white};
     }
 
     .react-datepicker__time-container {
@@ -85,11 +114,11 @@ export const datePickerCSS = css`
       .react-datepicker__time-box
       ul.react-datepicker__time-list
       li.react-datepicker__time-list-item {
-      background-color: ${({ theme }) => theme.colors.darkerGrey};
-      color: ${({ theme }) => theme.colors.creamDark};
+      background-color: ${({ theme }) => theme.colors.cream};
+      color: ${({ theme }) => theme.colors.primary};
 
       &:hover {
-        background-color: ${({ theme }) => theme.colors.primary};
+        background-color: ${({ theme }) => theme.colors.primaryDark};
       }
     }
 
