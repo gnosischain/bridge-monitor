865feed0de67e90da85a417d15e0545364d0ffd7
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-05-27 19:17:33 +0200

    text fix xdai->dai bridging success page

--- app/src/hooks/bridge/useTokenInfo.tsx
+++ app/src/hooks/bridge/useTokenInfo.tsx
@@ -5,19 +5,28 @@ import { chainsConfig } from '@/src/constants/config/chains'
 import { ERC20__factory } from '@/types/typechain'
 import { useBridgedTokens } from '@/src/providers/tokenListProvider'
 import { Token } from '@/types/token'
+import { NATIVE_TOKEN_ADDRESS } from '@/src/constants/config/common'
 
 export const useTokenInfo = (tokenAddress: string, chainId: ChainsValues) => {
   const { tokensByAddress } = useBridgedTokens()
 
   return useSWR([chainId, tokenAddress], async ([_chainId, _token]) => {
-    const provider = new JsonRpcBatchProvider(chainsConfig[_chainId].rpcUrl)
-    const tokenContract = ERC20__factory.connect(_token, provider)
+    if (tokenAddress.toLowerCase() === NATIVE_TOKEN_ADDRESS.toLowerCase() && chainId === 100) {
+      return {
+        name: 'XDAI',
+        symbol: 'XDAI',
+        decimals: 18,
+      } as Token
+    }
 
     const token = tokensByAddress[_token.toLowerCase()]
 
     if (token) {
       return token
     }
+
+    const provider = new JsonRpcBatchProvider(chainsConfig[_chainId].rpcUrl)
+    const tokenContract = ERC20__factory.connect(_token, provider)
 
     try {
       const [name, symbol, decimals] = await Promise.all([
