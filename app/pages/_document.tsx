eb04afe33f57e115366df89f6482c2bd4cd3a298
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-07-19 15:56:31 +0200

    add manifest

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
           <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
           <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/favicon/site.webmanifest" rel="manifest" />
+          <link href="/manifest.json" rel="manifest" />
           <link color="#333" href="/favicon/safari-pinned-tab.svg" rel="mask-icon" />
           <meta content="#333" name="msapplication-TileColor" />
           <meta content="#333" name="theme-color"></meta>
