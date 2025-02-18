790d05ff06b844772d00b4715a6384d1fe6371f9
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-02-18 11:18:33 +0100

    show amount in dialog

--- app/src/pagePartials/bridge/bridgeForm/bridgeWithSteps/index.tsx
+++ app/src/pagePartials/bridge/bridgeForm/bridgeWithSteps/index.tsx
@@ -7,6 +7,7 @@ import { Swap } from './Swap'
 import { Bridge } from './Bridge'
 import { Token } from '@/types/token'
 import { Step, steps } from './const'
+import { formatUnits } from 'ethers/lib/utils'
 
 const StatusList = styled.div`
   background-color: ${({ theme: { colors } }) => colors.cream};
@@ -42,11 +43,13 @@ export const BridgeWithSteps: React.FC<BridgeWithStepsProps> = ({
 }) => {
   const [status, setStatus] = useState<Step[]>(steps.approve)
 
+  const formattedAmount = formatUnits(amount, tokenIn?.decimals)
+
   return (
     <Modal
       onClose={onClose}
       size="md"
-      title={`Bridge ${tokenIn.symbol} from Gnosis Chain to ${tokenOut.symbol} Ethereum`}
+      title={`Bridge ${formattedAmount} ${tokenIn.symbol} from Gnosis Chain to ${tokenOut.symbol} Ethereum`}
       {...restProps}
     >
       <StatusList>
