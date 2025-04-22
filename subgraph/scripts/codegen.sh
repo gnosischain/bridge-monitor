a59d0153cb075a94aa2e0afacb8aaadbd6164cd1
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-04-22 11:24:11 +0200

    uncomment a cleaning part of codegen script

--- subgraph/scripts/codegen.sh
+++ subgraph/scripts/codegen.sh
@@ -4,10 +4,10 @@ set -e
 source "./scripts/env.sh"
 
 # # @TODO we might reuse the generated files only removing when TYPE is diff from previous
-# if [ -d "generated" ]; then
-#   echo "Cleaning generated types"
-#   rm -r generated/
-# fi
+if [ -d "generated" ]; then
+  echo "Cleaning generated types"
+  rm -r generated/
+fi
 
 VALIDATORS_TYPES_CONFIG="./src/config/validators.template"
 VALIDATORS_TYPES_OUTPUT="./src/config/validators.ts"
