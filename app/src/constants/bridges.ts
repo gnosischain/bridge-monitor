0d13356ee2e4b0c9f10fbbccf06d8ffffae165ac
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-07-04 14:49:28 +0200

    config info

--- app/src/constants/bridges.ts
+++ app/src/constants/bridges.ts
@@ -1,14 +1,19 @@
+import { Chains } from './config/chains'
+import { contracts } from './config/contracts'
+
 export const bridgeConfig = Object.freeze({
   XDAI: {
     bridgeProxy: '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016',
     governorMultisig: '0x42F38ec5A75acCEc50054671233dfAC9C0E7A3F6',
     tokens: {
       dai: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+      usds: '0xdc035d45d973e3ec169d2276ddab16f1e407384f',
     },
     protocol: {
       address: '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
       token: '0xc00e94cb662c3520282e6f5717214004a7f26888',
     },
+    bridgeRouter: contracts.BridgeRouter.address[Chains.mainnet],
   },
   OMNI: {
     bridgeProxy: '0x88ad09518695c6c3712AC10a214bE5109a655671',
