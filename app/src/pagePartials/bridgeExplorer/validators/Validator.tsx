b600f556d8a3aeb3b6dd007f696f9278bab5a9e8
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-08-25 13:43:28 +0200

    delete from validators

--- app/src/pagePartials/bridgeExplorer/validators/Validator.tsx
+++ app/src/pagePartials/bridgeExplorer/validators/Validator.tsx
@@ -13,7 +13,6 @@ import {
   TELEPATHY_VALIDATOR_ADDRESS_REPLACED,
 } from '@/src/constants/misc'
 import { IconLink } from '@/src/components/assets/IconLink'
-import { Tooltip } from '@/src/components/tooltip'
 
 const Wrapper = styled(InnerCard)`
   min-height: var(--validator-item-min-height);
@@ -123,17 +122,6 @@ const ExternalLinkWrapper = styled.span`
   column-gap: var(--theme-common-space);
 `
 
-const HashiTooltip = styled(Tooltip)`
-  flex-grow: 1;
-  margin-left: var(--theme-common-space);
-`
-
-const HashiTooltipWrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex-grow: 1;
-`
-
 const Column = styled.div`
   display: flex;
   flex-direction: column;
@@ -161,20 +149,6 @@ export const Validator: React.FC<Props> = ({ bridgeValidator, ...restProps }) =>
     return HealthStatusTypes.success
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const openLink = (e: any, href: string) => {
-    e.stopPropagation()
-    e.preventDefault()
-
-    window.open(href, '_blank', 'noopener noreferrer')
-  }
-
-  const isHashi = bridgeValidator.shortName === 'H'
-
-  // if (isHashi) {
-  //   return null
-  // }
-
   return (
     <Wrapper {...restProps}>
       <ValidatorHeader
@@ -189,89 +163,28 @@ export const Validator: React.FC<Props> = ({ bridgeValidator, ...restProps }) =>
         </Row>
         <Row>
           <Text>Signed (24hs)</Text>
-          {isHashi && (
-            <HashiTooltipWrapper>
-              <HashiTooltip content="A message is considered signed/approved by Hashi, when a threshold amount of oracles store the same message hash w.r.t the message id." />
-            </HashiTooltipWrapper>
-          )}
           <Value>{bridgeValidator.signed}</Value>
         </Row>
-        {isHashi ? (
-          <Row>
-            <Text>Executed (24hs)</Text>
-            <HashiTooltipWrapper>
-              <HashiTooltip content="Hashi only approves messages, the message is executed either by other bridge validators or by users." />
-            </HashiTooltipWrapper>
-            <Value>N/A</Value>
-          </Row>
-        ) : (
-          <Row>
-            <Text>Executed (24hs)</Text>
-            <Value>{bridgeValidator.executed}</Value>
-          </Row>
-        )}
+
+        <Row>
+          <Text>Executed (24hs)</Text>
+          <Value>{bridgeValidator.executed}</Value>
+        </Row>
       </Rows>
       <SubTitle>Balance</SubTitle>
       <Row>
-        {isHashi ? (
-          <>
-            <Text>N/A</Text>
-            <HashiTooltipWrapper>
-              <HashiTooltip content="Hashi don’t need to execute a message, hence balance field is not applicable." />
-            </HashiTooltipWrapper>
-          </>
-        ) : (
-          <Balance balanceType={balanceGnosis} />
-        )}
+        <Balance balanceType={balanceGnosis} />
       </Row>
-      {isHashi ? (
-        <Row>
-          <Text>Find more info</Text>
-          <Column className="text-right">
-            <ExternalLinkWrapper>
-              <ExternalLink href="https://hashi-explorer.xyz/" rel="noreferrer" target="_blank">
-                Hashi Explorer
-              </ExternalLink>
-              <Link
-                className="externalLink"
-                height={14}
-                onClick={(e) => openLink(e, 'https://hashi-explorer.xyz/')}
-                width={14}
-              />
-            </ExternalLinkWrapper>
-            <ExternalLinkWrapper>
-              <ExternalLink
-                href="https://docs.gnosischain.com/bridges/About%20Token%20Bridges/hashi-integration"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Docs
-              </ExternalLink>
-              <Link
-                className="externalLink"
-                height={14}
-                onClick={(e) =>
-                  openLink(
-                    e,
-                    'https://docs.gnosischain.com/bridges/About%20Token%20Bridges/hashi-integration',
-                  )
-                }
-                width={14}
-              />
-            </ExternalLinkWrapper>
-          </Column>
-        </Row>
-      ) : (
-        <Row>
-          <Text>Send tokens</Text>
-          <Address
-            address={validatorAddress}
-            characters={6}
-            copy
-            href={getAddressScanUrl(validatorAddress, bridgeValidator.scanUrl ?? 'gnosis')}
-          />
-        </Row>
-      )}
+
+      <Row>
+        <Text>Send tokens</Text>
+        <Address
+          address={validatorAddress}
+          characters={6}
+          copy
+          href={getAddressScanUrl(validatorAddress, bridgeValidator.scanUrl ?? 'gnosis')}
+        />
+      </Row>
     </Wrapper>
   )
 }
