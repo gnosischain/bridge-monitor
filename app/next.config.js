7c99b1d6f0bdb0692ef3108748994881f8139328
Wagalidoom <lukas-8@live.fr>
2026-01-05 20:20:34 +0100

    fix: csp

--- app/next.config.js
+++ app/next.config.js
@@ -7,9 +7,9 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data:;
-    font-src 'self';
-    connect-src 'self' https://indexer.hyperindex.xyz https://rpc.gnosischain.com/ https://rpc.ankr.com/eth https://tokens.coingecko.com/uniswap/all.json https://ethereum-rpc.publicnode.com/;
+    img-src 'self' blob: data: https://assets.coingecko.com;
+    font-src 'self' https://fonts.gstatic.com;
+    connect-src 'self' https://indexer.hyperindex.xyz https://rpc.gnosischain.com/ https://tokens.coingecko.com https://rpc.ankr.com/eth https://ethereum-rpc.publicnode.com/;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
