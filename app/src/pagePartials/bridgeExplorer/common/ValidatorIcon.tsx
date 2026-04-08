84389d07f3b20978c6e62cbba943734d424562bf
Wagalidoom <lukas-8@live.fr>
2026-04-08 09:35:08 +0200

    fix: linter

--- app/src/pagePartials/bridgeExplorer/common/ValidatorIcon.tsx
+++ app/src/pagePartials/bridgeExplorer/common/ValidatorIcon.tsx
@@ -34,24 +34,24 @@ export const ValidatorIcon: React.FC<Props> = ({ shortName, size, title, ...rest
     validator === 'S'
       ? { image: `${basePath}safe.svg`, size: 38, bgColor: '#12FF80' }
       : validator === 'H'
-      ? { image: `${basePath}hopr.svg`, size: 36, bgColor: '#EDE9EF' }
-      : validator === 'KL'
-      ? { image: `${basePath}kleros.svg`, size: 36, bgColor: '#EDE9EF' }
-      : validator === 'PF'
-      ? { image: `${basePath}protofire.svg`, size: 28, bgColor: '#121f3f' }
-      : validator === 'CP'
-      ? { image: `${basePath}cow-protocol.png`, size: 40, bgColor: '#052b65' }
-      : validator === 'GD'
-      ? { image: `${basePath}gnosis.svg`, size: 36, bgColor: '#fff' }
-      : validator === 'K'
-      ? { image: `${basePath}karpatkey.svg`, size: 36, bgColor: '#221F20' }
-      : validator === 'G'
-      ? { image: `${basePath}giveth.svg`, size: 30, bgColor: '#fff' }
-      : validator === 'GW'
-      ? { image: `${basePath}gateway.svg`, size: 36, bgColor: '#EDE9EF' }
-      : validator === 'TY'
-      ? { image: `${basePath}telepathy.svg`, size: 36, bgColor: '#fff' }
-      : { image: `${basePath}empty-token.png`, size: 40, bgColor: '#3E6957' }
+        ? { image: `${basePath}hopr.svg`, size: 36, bgColor: '#EDE9EF' }
+        : validator === 'KL'
+          ? { image: `${basePath}kleros.svg`, size: 36, bgColor: '#EDE9EF' }
+          : validator === 'PF'
+            ? { image: `${basePath}protofire.svg`, size: 28, bgColor: '#121f3f' }
+            : validator === 'CP'
+              ? { image: `${basePath}cow-protocol.png`, size: 40, bgColor: '#052b65' }
+              : validator === 'GD'
+                ? { image: `${basePath}gnosis.svg`, size: 36, bgColor: '#fff' }
+                : validator === 'K'
+                  ? { image: `${basePath}karpatkey.svg`, size: 36, bgColor: '#221F20' }
+                  : validator === 'G'
+                    ? { image: `${basePath}giveth.svg`, size: 30, bgColor: '#fff' }
+                    : validator === 'GW'
+                      ? { image: `${basePath}gateway.svg`, size: 36, bgColor: '#EDE9EF' }
+                      : validator === 'TY'
+                        ? { image: `${basePath}telepathy.svg`, size: 36, bgColor: '#fff' }
+                        : { image: `${basePath}empty-token.png`, size: 40, bgColor: '#3E6957' }
 
   return (
     <Wrapper bgColor={data.bgColor} size={size} {...restProps}>
