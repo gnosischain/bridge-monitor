476aa8c7822fea51823ad3325a5a8ceb23589a24
Wagalidoom <lukas-8@live.fr>
2026-08-21 11:14:01 +0200

    chore: remove swr from token list provider

--- app/src/providers/tokenListProvider.tsx
+++ app/src/providers/tokenListProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren, createContext, useContext } from 'react'
-import useSWR from 'swr/immutable'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import groupBy from 'lodash/groupBy'
 import { TokensLists } from '@/src/constants/config/types'
 import { getIcon } from '@/src/utils/icons'
@@ -89,65 +89,71 @@ const removeSpecialCharactersInName = (token: Token) => ({
   name: token.name.replace('//', ''),
 })
 
+const fetchTokenList = async (): Promise<TokenListQueryReturn> => {
+  // fetch all token from the constant TokenLists
+  const tokenListPromises = Object.values(TokensLists).map(async (url) => fetch(url))
+
+  // filter out the promises that are not fulfilled
+  const fulfilledResults = await Promise.allSettled(tokenListPromises).then((results) =>
+    results.filter(isFulfilled),
+  )
+
+  // fetch all token lists
+  const tokenLists: TokenListResponse[] = await Promise.all(
+    fulfilledResults.map((fulfilledResult) => {
+      if (fulfilledResult.value.ok) {
+        return fulfilledResult.value.json()
+      }
+
+      return Promise.resolve({ tokens: [] })
+    }),
+  )
+
+  // Unify all tokens from all token lists
+  const tokenList = tokenLists.flatMap((tokenList) => tokenList.tokens)
+  const addLogoUri = addLogoUriByTokenList(tokenList)
+
+  // fetch tokens from the bridge
+  const bridgedTokens = await fetchBridgedTokens()
+
+  const tokens = bridgedTokens.map((token) =>
+    addLogoUri(removeSpecialCharactersInName(lowerCaseAddresses(token))),
+  )
+
+  const isBridgedToNative = (token: Token) =>
+    isNativeToken(
+      (token.extensions.bridgeInfo[1]?.tokenAddress ??
+        token.extensions.bridgeInfo[100]?.tokenAddress) ||
+        zeroAddress,
+    )
+
+  return {
+    tokenList,
+    tokensByAddress: Object.fromEntries(tokens.map((token) => [token.address, token] as const)),
+    tokensByNetwork: groupBy(tokens, 'chainId'),
+    ambTokensByNetwork: groupBy(
+      tokens.filter((token) => !isBridgedToNative(token)),
+      'chainId',
+    ),
+    nativeTokensByNetwork: Object.fromEntries(
+      tokens
+        .filter((token) => isNativeToken(token.address))
+        .map((token) => [token.chainId, token] as const),
+    ),
+  }
+}
+
 /**
  * Custom hook that fetches and returns a list of tokens from various sources.
  * @returns An object containing the list of tokens, indexed by address, and network.
  */
-const useTokenListQuery = () => {
-  return useSWR(['token-list'], async (): Promise<TokenListQueryReturn> => {
-    // fetch all token from the constant TokenLists
-    const tokenListPromises = Object.values(TokensLists).map(async (url) => fetch(url))
-
-    // filter out the promises that are not fulfilled
-    const fulfilledResults = await Promise.allSettled(tokenListPromises).then((results) =>
-      results.filter(isFulfilled),
-    )
-
-    // fetch all token lists
-    const tokenLists: TokenListResponse[] = await Promise.all(
-      fulfilledResults.map((fulfilledResult) => {
-        if (fulfilledResult.value.ok) {
-          return fulfilledResult.value.json()
-        }
-
-        return Promise.resolve({ tokens: [] })
-      }),
-    )
-
-    // Unify all tokens from all token lists
-    const tokenList = tokenLists.flatMap((tokenList) => tokenList.tokens)
-    const addLogoUri = addLogoUriByTokenList(tokenList)
-
-    // fetch tokens from the bridge
-    const bridgedTokens = await fetchBridgedTokens()
-
-    const tokens = bridgedTokens.map((token) =>
-      addLogoUri(removeSpecialCharactersInName(lowerCaseAddresses(token))),
-    )
-
-    const isBridgedToNative = (token: Token) =>
-      isNativeToken(
-        (token.extensions.bridgeInfo[1]?.tokenAddress ??
-          token.extensions.bridgeInfo[100]?.tokenAddress) ||
-          zeroAddress,
-      )
-
-    return {
-      tokenList,
-      tokensByAddress: Object.fromEntries(tokens.map((token) => [token.address, token] as const)),
-      tokensByNetwork: groupBy(tokens, 'chainId'),
-      ambTokensByNetwork: groupBy(
-        tokens.filter((token) => !isBridgedToNative(token)),
-        'chainId',
-      ),
-      nativeTokensByNetwork: Object.fromEntries(
-        tokens
-          .filter((token) => isNativeToken(token.address))
-          .map((token) => [token.chainId, token] as const),
-      ),
-    }
+const useTokenListQuery = () =>
+  useSuspenseQuery({
+    queryKey: ['token-list'],
+    queryFn: fetchTokenList,
+    staleTime: Infinity,
+    gcTime: Infinity,
   })
-}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const TokenListContext = createContext<TokenListQueryReturn>({} as any)
@@ -155,9 +161,7 @@ const TokenListContext = createContext<TokenListQueryReturn>({} as any)
 export const TokenListProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
   const { data } = useTokenListQuery()
 
-  return data ? (
-    <TokenListContext.Provider value={data}>{children}</TokenListContext.Provider>
-  ) : null
+  return <TokenListContext.Provider value={data}>{children}</TokenListContext.Provider>
 }
 
 export default TokenListProvider
