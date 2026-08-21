fc0cbf839fa808825eb31780e9a74b49ce3f470c
Wagalidoom <lukas-8@live.fr>
2026-08-21 11:39:03 +0200

    chore: remove swr from useValidators and validatorsProvider

--- app/src/hooks/useValidators.ts
+++ app/src/hooks/useValidators.ts
@@ -1,27 +1,24 @@
-import useSWR from 'swr'
+import { useSuspenseQuery } from '@tanstack/react-query'
 
 import { BridgesValues } from '@/src/constants/config/bridges'
 import { fetchExecutedTransactions, fetchSignedTransactions } from '@/src/utils/validators'
 
 export const useFetchValidatorsSignatures = (bridge: BridgesValues, afterDate: number) => {
-  const {
-    data,
-    error,
-    mutate: refetch,
-  } = useSWR(['useFetchSignedTransactions', bridge, afterDate.toString()], () =>
-    fetchSignedTransactions(bridge, afterDate),
-  )
+  const { data, error, refetch } = useSuspenseQuery({
+    queryKey: ['useFetchSignedTransactions', bridge, afterDate.toString()],
+    queryFn: () => fetchSignedTransactions(bridge, afterDate),
+    staleTime: 60_000,
+  })
 
   return { data, error, refetch }
 }
 
 export const useFetchValidatorsExecutions = (bridge: BridgesValues, afterDate: number) => {
-  const {
-    data,
-    error,
-    mutate: refetch,
-  } = useSWR(['useFetchExecutedTransactions', bridge, afterDate.toString()], () =>
-    fetchExecutedTransactions(bridge, afterDate),
-  )
+  const { data, error, refetch } = useSuspenseQuery({
+    queryKey: ['useFetchExecutedTransactions', bridge, afterDate.toString()],
+    queryFn: () => fetchExecutedTransactions(bridge, afterDate),
+    staleTime: 60_000,
+  })
+
   return { data, error, refetch }
 }
